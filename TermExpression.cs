1fbf265eb92664e6296e88adbff119d7eda5f098
ugsk\NikonovAV <NikonovAV@ugsk.ru>
2016-09-26 16:40:29 +0500

    add some code

--- TermExpression.cs
+++ TermExpression.cs
@@ -8,10 +8,40 @@ namespace GeneticRegularGenerator
 {
     public class TermExpression
     {
-        public string Expression { get; internal set; }
+        public string Expression {
+            get 
+            {
+                return ParseNonTerminal(ExpressionTokens, 0);
+            }
+            internal set;
+        }
 
         public Token[] ExpressionTokens { get; set; }
 
         public double FithessFactor { get; set; }
+
+        private string ParseNonTerminal(Token[] expression, int position)
+        {
+            string result = "";
+            var length = ExpressionTokens.Length;
+            var arityCounter = expression[position].Arity;
+
+            result += expression[position]; 
+
+            while (arityCounter > 0)
+            {
+                if (ExpressionTokens[position].IsTerminal)
+                {
+                    result += ExpressionTokens[position];
+                }
+                else
+                {
+                    result += ParseNonTerminal(ExpressionTokens, position);
+                }
+
+                arityCounter--;
+            }
+            return string.Format(result, arguments);
+        }
     }
 }
