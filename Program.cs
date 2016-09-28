7518baa10097506038bba243c9585ef523890e38
ugsk\NikonovAV <NikonovAV@ugsk.ru>
2016-09-28 16:46:22 +0500

    Добавил измерение времени выполнения

--- Program.cs
+++ Program.cs
@@ -10,43 +10,69 @@ namespace GeneticRegularGenerator
 {
     class Program
     {
+        static long mx = 0;
+        static long mn = 0;
+
+        static int counter = 0;
+        static long accumTime = 0;
+
         static void Main(string[] args)
         {
             var testWord = "Самарская обл., Безенчукский р-н, пгт. Безенчук, ул. Квартальная";
+            var testWord2 = "Самарская обл., Безенчукский р-н, пгт. Безенчук, ул. Мира";
 
             var expected = "Квартальная";
+            var expected2 = "Мира";
 
             Func<string, double> fitnessFunction = rxp =>
             {
-                var fithessWeight = 0d;
-
-                Regex regex = null;
-                Match match = null;
-
-                try
-                {
-                    regex = new Regex(rxp, RegexOptions.IgnoreCase, TimeSpan.FromSeconds(10));
-                    match = regex.Match(testWord);
-                }
-                catch
-                {
-                    return -1000;
-                }
-
-                if (match.Success)
-                {
-                    var sim = Levenshtein.GetSimilarity(match.Value, expected);
-                    fithessWeight += sim * 1000;
-                }
-
-                return fithessWeight;
+                return (Calculate(rxp, testWord, expected) + Calculate(rxp, testWord2, expected2))/2;
             };
 
             var generator = new RegularExpressionGenerator(fitnessFunction);
 
-            generator.Run(10, 20);
+            generator.Run(11, 20);
 
             Console.WriteLine(generator.TheBestExpression.Expression);
+        }
+
+        private static double Calculate(string expression, string testWord, string expected) 
+        {
+            counter++;
+
+            var fithessWeight = 0d;
+
+            Regex regex = null;
+            Match match = null;
+
+            var startTime = DateTime.Now.Ticks;
+            try
+            {
+                regex = new Regex(expression, RegexOptions.IgnoreCase, TimeSpan.FromSeconds(10));
+                match = regex.Match(testWord);
+            }
+            catch
+            {
+                return -1000;
+            }
+
+            var deltaTicks = DateTime.Now.Ticks - startTime;
+            if (mx < deltaTicks) mx = deltaTicks;
+            //if (mn > deltaTicks) mn = deltaTicks;
+
+            accumTime += deltaTicks;
+
+            if (accumTime / 200 > deltaTicks) fithessWeight += 10;
+
+            if (counter > 199) mx = 0;
+
+            if (match.Success)
+            {
+                var sim = Levenshtein.GetSimilarity(match.Value, expected);
+                fithessWeight += sim * 1000;
+            }
+
+            return fithessWeight;
         }
     }
 
